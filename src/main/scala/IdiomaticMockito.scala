041c2fd08ec65ecd1b0a64012e132f587741da09
dethstrobe <dethstrobe@gmail.com>
2020-09-01 11:20:52 -0400

    getting mockito working

---
+++ src/main/scala/IdiomaticMockito.scala
@@ -0,0 +1,56 @@
+package org.mockito.scalatest
+
+import org.mockito.PostfixVerifications
+import org.mockito.ArgumentMatchersSugar
+import org.mockito.IdiomaticVerifications
+import org.scalatest.Succeeded
+import org.scalatest.compatible.Assertion
+
+trait IdiomaticMockitoBase
+    extends org.mockito.IdiomaticStubbing
+    with ArgumentMatchersSugar
+    with MockitoSessionFixture { this: IdiomaticVerifications =>
+  override type Verification = Assertion
+  override def verification(v: => Any): Verification = {
+    v
+    Succeeded
+  }
+}
+
+trait IdiomaticMockito extends IdiomaticMockitoBase with PostfixVerifications {
+  override type Verification = Assertion
+  override def verification(v: => Any): Verification = {
+    v
+    Succeeded
+  }
+}
+
+trait AsyncIdiomaticMockitoBase
+    extends org.mockito.IdiomaticStubbing
+    with ArgumentMatchersSugar
+    with MockitoSessionAsyncFixture { this: IdiomaticVerifications =>
+  override type Verification = Assertion
+  override def verification(v: => Any): Verification = {
+    v
+    Succeeded
+  }
+}
+
+trait AsyncIdiomaticMockito
+    extends AsyncIdiomaticMockitoBase
+    with PostfixVerifications {
+  override type Verification = Assertion
+  override def verification(v: => Any): Verification = {
+    v
+    Succeeded
+  }
+}
+
+trait MockitoSugar
+    extends org.mockito.MockitoSugar
+    with ArgumentMatchersSugar
+    with MockitoSessionFixture
+trait AsyncMockitoSugar
+    extends org.mockito.MockitoSugar
+    with ArgumentMatchersSugar
+    with MockitoSessionAsyncFixture
---
+++ src/test/scala/IdiomaticMockitoTest.scala
@@ -0,0 +1,25 @@
+package user.org.mockito.scalatest
+
+import org.mockito.scalatest.AsyncIdiomaticMockito
+
+import scala.concurrent.Future
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AsyncWordSpec
+
+class IdiomaticMockitoTest
+    extends AsyncWordSpec
+    with Matchers
+    with AsyncIdiomaticMockito {
+  "AsyncMockito" should {
+    "work with specialised methods" in {
+      val mockFunction = mock[() => Int]
+      mockFunction() returns 42
+
+      Future(mockFunction.apply())
+        .map { v =>
+          v shouldBe 42
+          mockFunction() was called
+        }
+    }
+  }
+}
